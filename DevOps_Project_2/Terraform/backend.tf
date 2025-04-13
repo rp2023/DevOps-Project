24b6d028610c232d3b5245796a9b9893544a6284
rp2023 <122508152+rp2023@users.noreply.github.com>
2025-04-13 11:36:23 +0530

    Update backend.tf

--- DevOps_Project_2/Terraform/backend.tf
+++ DevOps_Project_2/Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "euran-terraform-statefile"
+    bucket = "euran-terraform-statefile2025"
     key = "server_name/statefile"
     region = "ap-south-1"
   }
